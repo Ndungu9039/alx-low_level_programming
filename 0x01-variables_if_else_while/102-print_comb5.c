2c9fb819bd52441262b478739134d528142afcab
Ndungu9039 <lawrencegithinji6@gmail.com>
2022-03-11 16:02:10 -0800

    Modified file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,14 +8,21 @@
  */
 int main(void)
 {
-int d;
+int num1, num2;
 
-for (d = 0; d < 100; d++)
+for (numm1 = 0; num1 <= 98; num1++)
 {
-putchar((d / 10) + '0');
-putchar((d % 10) + '0');
-if (d != 99)
+for (num2 = num1 + 1; num2 <= 99; num2++)
 {
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
+putchar(' ');
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
+
+if (num1 == 98 && num2 == 99)
+continue;
+
 putchar(',');
 putchar(' ');
 }
