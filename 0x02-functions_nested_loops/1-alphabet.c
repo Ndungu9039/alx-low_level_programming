d61403d25ba28afba4b404f2a9b8bd7db3a2216e
Ndungu9039 <lawrencegithinji6@gmail.com>
2022-03-15 16:42:02 +0000

    modified file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,7 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+void print_alphabet(void);
+int main ()
 {
 int n;
 
@@ -14,4 +15,6 @@ for (n = 'a'; n <= 'z'; n++)
 _putchar(n);
 }
 _putchar('\n');
+
+return (0);
 }
