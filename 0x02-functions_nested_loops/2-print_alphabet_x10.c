b99645fe39fec0ee66e0bd7031065975d78269dd
Ndungu9039 <lawrencegithinji6@gmail.com>
2022-03-15 11:45:57 +0000

    Modified file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,14 @@
  * Return: Nothing
  */
 
-void print_alphabet_x10(void)
+void print_alphabet_x10()
 {
 char i;
 int j;
 
 for (j = 0; j < 10; j++)
 {
-for (i ='a'; i <= 'z'; i++)
+for (i = 'a'; i <= 'z'; i++)
 _putchar(i);
 _putchar('\n');
 }
