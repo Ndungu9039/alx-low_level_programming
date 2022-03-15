57c70d9a7da9888236d0ea71c857b1ee309ef861
Ndungu9039 <lawrencegithinji6@gmail.com>
2022-03-15 11:38:57 +0000

    2. 10 xalphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Nothing
+ */
+
+void print_alphabet_x10(void)
+{
+char i;
+int j;
+
+for (j = 0; j < 10; j++)
+_putchar(i);
+_putchar('\n');
+}
+}
