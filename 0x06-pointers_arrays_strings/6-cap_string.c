c2d8ba51c64604c985b3a33c16ef35b5adaeeace
Ndungu9039 <lawrencegithinji6@gmail.com>
2022-03-23 20:42:21 +0000

    capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,7 +4,6 @@
 /**
  * *cap_string - capitalizes all words
  * @s: pointer to char parameters
- *
  * Return: *s
  */
 
@@ -16,7 +15,7 @@ char delimeters[] = " \t\n,;.!?\"(){}";
 for (i = 0; s[i] != '\0'; i++)
 {
 if (s[0] >= 97 && s[0] <= 122)
-s[0] = s[0] -32;
+s[0] = s[0] - 32;
 for (j = 0; delimeters[j] != '\0'; j++)
 if (s[i] == delimeters[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
 s[i + 1] = s[i + 1] - 32;
