e724c6102a1d22d57013e76bf3b5aa2778e9d35b
Ndungu9039 <lawrencegithinji6@gmail.com>
2022-03-28 17:26:08 +0000

    prints chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,18 +3,18 @@
 /**
  * print_chessboard - prints chessboard.
  * @a: array
- * Return: Success (0)
  */
 
 void print_chessboard(char (*a)[8])
 {
-int i;
-int j;
+int i, j;
 
 for (i = 0; i < 8; i++)
 {
 for (j = 0; j < 8; j++)
+{
 _putchar(a[i][j]);
+}
 _putchar('\n');
 }
 }
