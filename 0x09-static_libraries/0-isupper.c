10f46e5d1ef75bb61d5ecf7c97aa6fcc45b74738
Ndungu9039 <lawrencegithinji6@gmail.com>
2022-04-01 18:30:46 +0000

    static library libmy.a

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks the code for Holberton School students.
+ *
+ * @c: int parameter
+ *
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+
+return (c >= 65 && c <= 90);
+}
