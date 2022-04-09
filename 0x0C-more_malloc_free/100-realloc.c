d902ff1bad1e5fbfe6ecfceaba1edf8d6bfe3b80
Ndungu9039 <lawrencegithinji6@gmail.com>
2022-04-09 10:51:00 +0000

    _putchar file

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,40 +13,27 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-char *ptr1;
-char *old_ptr;
-unsigned int i;
+char *s;
 
+{
+if (new_size > old_size)
+s = malloc(new_size);
+free(ptr);
+}
 if (new_size == old_size)
+{
 return (ptr);
-
+}
+if (ptr == NULL)
+{
+s = malloc(new_size);
+free(ptr);
+return (s);
+}
 if (new_size == 0 && ptr)
 {
 free(ptr);
 return (NULL);
 }
-
-if (!ptr)
-return (malloc(new_size));
-
-ptr1 = malloc(new_size);
-if (!ptr1)
-return (NULL);
-
-old_ptr = ptr;
-
-if (new_size < old_size)
-{
-for (i = 0; i < new_size; i++)
-ptr1[i] = old_ptr[i];
-}
-
-if(new_size > old_size)
-{
-for (i = 0; i < old_size; i++)
-ptr1[i] = old_ptr[i];
-}
-
-free(ptr);
-return (ptr1);
+return (ptr);
 }
